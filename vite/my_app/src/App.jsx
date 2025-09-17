3fd2bf779844abb5b00a0fc34edd6464711701e7
dhanyaritgithub <dhanyashreealva@gmail.com>
2025-09-17 20:21:15 +0530

    components

---
+++ vite/my_app/src/App.jsx
@@ -0,0 +1,22 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import './App.css'
+
+
+function App() {
+  
+
+  return (
+    <>
+      <h1>
+        Welcome to everyone
+      </h1>
+      <p>Hello, how are you?</p>
+      <h2>React is interesting</h2>
+      <Greeting/>
+   </>
+  )
+}
+
+export default App
